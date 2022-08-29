abc628fcf999e4a4fdce568d11fba6930e1b6070
Docmine17 <67164552+Docmine17@users.noreply.github.com>
2022-08-29 20:38:36 -0300

    now with GUI!
    now with GUI!

--- OPLServer4Linux.sh
+++ OPLServer4Linux.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-                         
+#!/bin/sh
+
 #                @%*+=:.                 
 #                @@@@@@@@*=.             
 #                @@@@@#*%@@@%-           
@@ -17,5 +17,100 @@
 #
 #	      OPLServer4Linux
 #
-cd '/mnt/Disco (D)/Aplicativos/Roms/Sony - PS2/'
-Xvfb :9 -screen 0 1024x768x16 | DISPLAY=:9 flatpak run --env='WINEPREFIX=/mnt/Disco (D)/Aplicativos/Roms/Sony - PS2/.wine4opl' org.winehq.Wine '/mnt/Disco (D)/Aplicativos/Roms/Sony - PS2/OPLServer.exe' /START /NOLOG
+
+wine=false
+wget=false
+tar=false
+
+tmp=/tmp/oplserver4linux
+serverdir=$HOME/.local/share/oplserver4linux
+prefix=$HOME/.local/share/oplserver4linux/wineprefix
+autostart=$HOME/.config/autostart
+applications=$HOME/.local/share/applications
+icons=$HOME/.local/share/icons/hicolor/64x64/apps
+
+desktop="[Desktop Entry]\nVersion=1.1\nType=Application\nName=OPLServer\nIcon=icon64\nExec=$HOME/.local/share/oplserver4linux/opl.sh\nActions=\nCategories=Game;\nStartupWMClass=oplserver.exe"
+
+executable='#!/bin/sh \nIsRunning=$( ps -d | grep -c OPLServer.exe ) \nif [ $IsRunning -ne 0 ]; then \nzenity --info --text="O OPLServer já está em execução" \nelse \nserverdir=$HOME/.local/share/oplserver4linux \nprefix=$HOME/.local/share/oplserver4linux/wineprefix \nWINEPREFIX=$prefix wine $serverdir/OPLServer.exe /SILENT /HIDE /NOLOG /START\nfi'
+
+#Verificar comandos
+cmdtest() {
+	cmdexist=$( command -v $1 )
+	if [ $cmdexist ]; then
+	   echo "$1 installed"
+	else
+	   echo "$1 is not installed."
+	fi
+}
+
+#Verificar se as dependencias estão instaladas
+
+cmdtest wine
+cmdtest wget
+cmdtest tar
+
+ activity=$(zenity --list \
+  --title="OPLServer4Linux" \
+  --radiolist="Activities" --column="" --column="" \
+    1 "Install" \
+    2 "Start On Login" \
+    3 "Remove" \
+)
+
+case "$activity" in
+    "Install")
+    
+    echo "Installation its starting..."
+    echo "Baixando OPLServer"
+    mkdir $tmp
+    cd $tmp
+    wget "https://github.com/elmariolo/OPL-Server/releases/download/v2.0/v2.0.2022-08-24.tar"
+    wget -P $icons "https://raw.githubusercontent.com/elmariolo/OPL-Server/master/OPL_Server/OPLServer/icon64.png"
+    
+    if [ $? -eq 0 ]; then
+    	mkdir $serverdir
+    	tar -xf v2.0.2022-08-24.tar -C $serverdir
+    	
+    	if [ $? -eq 0 ]; then
+    		WINEPREFIX=$prefix winepath
+    		rm $applications/OPLServer4Linux.desktop
+    		mkdir $applications
+    		echo -e $executable >> $serverdir/opl.sh
+    		chmod +x $serverdir/opl.sh
+    		echo -e $desktop >> $applications/OPLServer4Linux.desktop
+    		xdg-icon-resource forceupdate
+    		zenity --info --text="OPLServer4Linux instalado com sucesso."
+    		#WINEPREFIX=$prefix wine $serverdir/OPLServer.exe
+    		
+    	else
+    		zenity --info --text="Falha na extração do aquivo"
+    	fi
+    else
+    	zenity --info --text="Falha no download do OPLServer"
+    	
+    fi
+    
+    ;;
+    
+    "Start On Login")
+    
+    rm $autostart/OPLServer4Linux.desktop
+    mkdir $autostart
+    echo -e $desktop >> $autostart/OPLServer4Linux.desktop
+    zenity --info --text="OPLServer4Linux adicionado ao Login."
+    ;;
+    
+    "Remove")
+    echo "Remove"
+    pkill OPLServer.exe
+    rm -R $tmp
+    rm -R $serverdir
+    rm $autostart/OPLServer4Linux.desktop
+    rm $applications/OPLServer4Linux.desktop
+    rm $icons/icon64.png
+    zenity --info --text="OPLServer4Linux Removido com sucesso."
+    ;;
+    
+esac
+
+#echo fim
